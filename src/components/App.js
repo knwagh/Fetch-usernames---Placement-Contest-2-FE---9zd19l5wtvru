362d3aeae20999a1d0970c5e109fa6ffb0ed4100
knwagh <107362567+knwagh@users.noreply.github.com>
2022-10-09 15:22:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,18 @@ import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
+ const [id,setId] = useState(1)
+ const [name,setName]= useState('');
+ const changeInput = (e) =>{
+  setId(e.target.value);
+ }
+ 
+ useEffect(()  =>{
+  fetch('https://content.newtonschool.co/v1/pr/main/users/${id}')
+  .then(res => res.json())
+  .then(data => setName(data.name))
+  .catch(e => console.log(e.message))
+ },[id]); 
 //code here 
  
 
